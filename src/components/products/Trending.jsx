f8092f7a11bed8bbc158ddb910d83bc33fcd308c
jaman hasan <jamanhasan246@gmail.com>
2025-05-08 14:55:11 +0600

    i have some modify in global.css like font side & other

--- src/components/products/Trending.jsx
+++ src/components/products/Trending.jsx
@@ -6,7 +6,7 @@ const Trending = ({product}) => {
     <div className="flex p-3 items-center gap-3 border border-gray-100">
     <Image className="w-10 h-10 object-cover" src={image} width={500} height={500} alt={title}  />
     <div className="truncate flex-1">
-      {title.length > 20 ? `${title.slice(0, 20)}...` : title}
+    <h4 className="h6">    {title.length > 20 ? `${title.slice(0, 20)}...` : title}</h4>
       <div className="text-sm font-semibold">${price}</div>
     </div>
   </div>
